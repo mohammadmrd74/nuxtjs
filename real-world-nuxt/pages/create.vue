ca2feb00bd3d747c1a6fd16b6c27bac1b5163743
Mohammadhasan <mohammadmrd74@gmail.com>
2020-03-21 05:18:05 +0430

    new

---
+++ real-world-nuxt/pages/create.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>
+      create an events  
+    </h1>
+  </div>
+</template>
---
+++ real-world-nuxt/pages/posts/_id.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>
+      this a post {{id}}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name : 'as',
+  data:function() {
+    return {
+      id: this.$route.params.id,
+      posts: [
+        {
+        id: '1',
+        content: 'ali'
+      },
+      {
+        id: '2',
+        content: 'ali2'
+      },
+      {
+        id: '3',
+        content: 'ali3'
+      },
+      ]
+    }
+  },
+  created (){
+    console.log(this.id);
+    
+  }
+}
+</script>
